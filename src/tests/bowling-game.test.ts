b8c3af9f37367551a343362d576743a3e5037084
JMendioroz <jmendiorozredin@gmail.com>
2026-06-20 16:57:32 +0200

    add initial tests

---
+++ src/tests/bowling-game.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from '@jest/globals';
+
+class BowlingGame {
+  rolls: number[] = [];
+
+  roll(pins: number) {
+    this.rolls.push(pins);
+  }
+
+  calculateTotalScore() {
+    return this.rolls.reduce((total, roll) => total + roll, 0);
+  }
+}
+
+describe('bowling game', () => {
+  it('should be able to create a game', () => {
+    const game = new BowlingGame();
+    expect(game).toBeInstanceOf(BowlingGame);
+  });
+
+  it('should be able to roll a ball', () => {
+    const game = new BowlingGame();
+    game.roll(0);
+    expect(game.rolls).toStrictEqual([0]);
+  });
+
+  it('should be able to calculate the total score for the worst game', () => {
+    const game = new BowlingGame();
+    rollMany(game, 20, 0);
+    expect(game.calculateTotalScore()).toBe(0);
+  });
+
+  it('should be able to calculate the total score for game with all ones', () => {
+    const game = new BowlingGame();
+    rollMany(game, 20, 1);
+    expect(game.calculateTotalScore()).toBe(20);
+  });
+});
+
+function rollMany(game: BowlingGame, times: number, pins: number) {
+  Array.from({ length: times }).forEach(() => game.roll(pins));
+}
