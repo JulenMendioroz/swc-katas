e62792ae21787d2b0d711c7e528b0ed7f1522d60
JMendioroz <jmendiorozredin@gmail.com>
2026-06-29 00:03:04 +0200

    add game grid

--- src/game-of-life/game-of-life.test.ts
+++ src/game-of-life/game-of-life.test.ts
@@ -41,7 +41,89 @@ class Cell {
   }
 }
 
+type Coord = [x: number, y: number];
+
+class Grid {
+  private used = new WeakSet<Cell>();
+  private grid = new Map<string, Cell>();
+
+  place(coord: Coord, cell: Cell) {
+    if (this.isOccupied(coord)) {
+      throw new Error();
+    }
+    if (this.isUsed(cell)) {
+      throw new Error();
+    }
+    this.use(cell);
+    this.grid.set(coord.toString(), cell);
+  }
+
+  private use(cell: Cell) {
+    this.used.add(cell);
+  }
+
+  private isUsed(cell: Cell) {
+    return this.used.has(cell);
+  }
+
+  private isOccupied(coord: Coord) {
+    return this.grid.has(coord.toString());
+  }
+
+  at(coord: Coord) {
+    return this.grid.get(coord.toString());
+  }
+
+  adjacent([x, y]: Coord) {
+    // prettier-ignore
+    const masks: Coord[] = [
+      [-1, -1], [0, -1], [1, -1],
+      [-1,  0],          [1,  0],
+      [-1,  1], [0,  1], [1,  1],
+    ];
+    return masks.map(([maskX, maskY]) => this.at([x + maskX, y + maskY])).filter(Boolean) as Cell[];
+  }
+}
+
 describe('The game of life', () => {
+  describe('grid', () => {
+    it('can place and retrieve cells', () => {
+      const grid = new Grid();
+      const coord = [0, 0] as Coord;
+      const cell = Cell.alive();
+      grid.place(coord, cell);
+      expect(grid.at(coord)).toBe(cell);
+    });
+    it('cannot place the same cell in different cords', () => {
+      const grid = new Grid();
+      const cell = Cell.alive();
+      grid.place([0, 0], cell);
+      expect(() => grid.place([0, 1], cell)).toThrow();
+    });
+    it('cannot place a cell in an occupied coord', () => {
+      const grid = new Grid();
+      grid.place([0, 0], Cell.alive());
+      expect(() => grid.place([0, 0], Cell.alive())).toThrow();
+    });
+    it('can have empty cells', () => {
+      const grid = new Grid();
+      expect(grid.at([0, 0])).toBeUndefined();
+    });
+    it('can find adjacent cells', () => {
+      const grid = new Grid();
+      const centerCoord = [0, 0] as Coord;
+      const coords: Coord[] = [centerCoord, [1, 0], [0, 1], [1, 1]];
+      coords.forEach((coord) => grid.place(coord, Cell.alive()));
+
+      const centerCell = grid.at(centerCoord);
+      const adjacent = grid.adjacent(centerCoord);
+
+      // TODO: test with non-adjacent cells and check only adjacent are returned
+      expect(centerCell).toBeDefined();
+      expect(adjacent).not.toContain(centerCell);
+      expect(new Set(adjacent).size).toBe(3);
+    });
+  });
   describe('cells', () => {
     it('can be alive', () => {
       const cell = Cell.alive();
